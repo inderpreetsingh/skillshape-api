0f481dcda5a8b059414d8966802701a5a92e3435
ramesh.bansal@daffodilsw.com <ramesh.bansal@daffodilsw.com>
2019-04-09 10:49:49 +0530

    new changes added

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-09T05:02:55.523Z",
+    "time": "2019-04-09T05:18:56.066Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
